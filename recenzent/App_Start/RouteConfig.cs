3c23ddb805b67d90e4616ca6c99a44903ed22bfc
Pawel <dziedzicpm@gmail.com>
2017-04-19 11:56:57 +0200

    początki panelu autora, kilka zmian w layoutcie

--- recenzent/App_Start/RouteConfig.cs
+++ recenzent/App_Start/RouteConfig.cs
@@ -18,6 +18,7 @@ namespace recenzent
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+
         }
     }
 }
---
+++ recenzent/Controllers/AuthorPanelController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.IO;
+
+namespace recenzent.Controllers
+{
+    public class AuthorPanelController : Controller
+    {
+        // GET: AuthorPanel
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult AddPub() {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddPub(HttpPostedFileBase file) {
+
+            if(file != null) {
+                string path = Server.MapPath("~/Uploads/");
+                if (!Directory.Exists(path)) {
+                    Directory.CreateDirectory(path);
+                }
+
+                file.SaveAs(path + Path.GetFileName(file.FileName));
+                ViewBag.Message = "Plik przesłany";
+            }
+
+            return View();
+        }
+    }
+}
